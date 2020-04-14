6a294c8fde709826c3039e3742d875825ca01bd9
yiwang <wangyi@ghcchina.com.cn>
2020-04-14 16:59:29 +0800

    add spring cloud demo project

---
+++ springboot-06-springcloud/provider-ticket/src/main/java/com/yiwang/providerticket/service/TicketService.java
@@ -0,0 +1,11 @@
+package com.yiwang.providerticket.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TicketService {
+
+    public String getTicket(){
+        return "《九品芝麻官》";
+    }
+}
